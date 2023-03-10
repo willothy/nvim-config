dc1d95ca1f1582e74d71e3151291a375220d9b1b
Will Hopkins <willothyh@gmail.com>
2023-03-10 02:43:15 -0800

    Added flatten, updated unception

---
+++ lua/plugins/flatten.lua
@@ -0,0 +1,33 @@
+return { {
+	'willothy/flatten.nvim',
+	dir = '~/projects/lua/flatten/',
+	opts = {
+		callbacks = {
+			pre_open = function()
+				require("toggleterm").toggle(0)
+			end,
+			post_open = function(bufnr, winnr, ft)
+				if ft == "gitcommit" then
+					vim.api.nvim_create_autocmd("FileChangedShell", {
+						buffer = bufnr,
+						once = true,
+						callback = function()
+							vim.api.nvim_buf_delete(bufnr, {
+								-- unload = true
+							})
+							-- vim.defer_fn(function()
+							-- 	vim.api.nvim_buf_delete(bufnr, {})
+							-- end, 500)
+						end
+					})
+				else
+					require("toggleterm").toggle(0)
+					vim.api.nvim_set_current_win(winnr)
+				end
+			end,
+			block_end = function()
+				require("toggleterm").toggle(0)
+			end
+		}
+	},
+} }
--- lua/plugins/unception.lua
+++ lua/plugins/unception.lua
@@ -3,6 +3,7 @@ return { {
 	config = function()
 		vim.g.unception_open_buffer_in_new_tab = true
 	end,
+	enabled = false,
 	init = function()
 		vim.api.nvim_create_autocmd(
 			"User",
@@ -11,34 +12,32 @@ return { {
 				callback = function()
 					-- Toggle the terminal off.
 					require('toggleterm').toggle(0)
-					local ft = vim.bo.filetype
-					if ft == "gitcommit" then
-						local autoreturn
-						autoreturn = vim.api.nvim_create_autocmd("BufWritePost", {
-							pattern = "gitcommit",
-							callback = function()
-								-- Toggle the terminal back on.
-								-- local winnr = vim.api.nvim_get_current_win()
-								-- local bufnr = vim.api.nvim_get_current_buf()
-								vim.api.nvim_buf_delete(0, {})
-								require('toggleterm').toggle(0)
-								-- vim.api.nvim_set_current_win(winnr)
-								vim.api.nvim_del_autocmd(autoreturn)
-							end
-						})
-					else
-						local autoenter
-						autoenter = vim.api.nvim_create_autocmd("BufEnter", {
-							pattern = "*",
-							callback = function()
-								-- Toggle the terminal back on.
-								local winnr = vim.api.nvim_get_current_win()
-								require('toggleterm').toggle(0)
-								vim.api.nvim_set_current_win(winnr)
-								vim.api.nvim_del_autocmd(autoenter)
-							end
-						})
-					end
+
+					-- vim.api.nvim_create_autocmd("BufEnter", {
+					-- 	pattern = "*",
+					-- 	once = true,
+					-- 	callback = function()
+					-- 		-- Toggle the terminal back on.
+					-- 		local winnr = vim.api.nvim_get_current_win()
+					-- 		local bufnr = vim.api.nvim_get_current_buf()
+					--
+					-- 		local ft = vim.bo.filetype
+					-- 		if ft == "gitcommit" then
+					-- 			vim.api.nvim_create_autocmd("BufWritePost", {
+					-- 				buffer = bufnr,
+					-- 				once = true,
+					-- 				callback = function()
+					-- 					
+					-- 					vim.api.nvim_buf_delete(bufnr, {})
+					-- 					-- require('toggleterm').toggle(0)
+					-- 				end
+					-- 			})
+					-- 		else
+					-- 			require('toggleterm').toggle(0)
+					-- 			vim.api.nvim_set_current_win(winnr)
+					-- 		end
+					-- 	end
+					-- })
 				end
 			}
 		)
