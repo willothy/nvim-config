ff0fd8f9a839ab331e10dc369ecb1c6f7f9b0c59
Will Hopkins <willothyh@gmail.com>
2023-06-28 17:31:32 -0700

    improve session / project management

--- lua/plugins/core/resession.lua
+++ lua/plugins/core/resession.lua
@@ -2,6 +2,10 @@ return {
   {
     "stevearc/resession.nvim",
     config = function()
+      local curtab = vim.api.nvim_get_current_tabpage
+      local tabnr = vim.api.nvim_tabpage_get_number
+      local bufname = vim.api.nvim_buf_get_name
+      local cwd = vim.fn.getcwd
       local resession = require("resession")
 
       resession.setup({
@@ -10,6 +14,9 @@ return {
           interval = 300,
           notify = false,
         },
+        tab_buf_filter = function(tabpage, bufnr)
+          return vim.startswith(bufname(bufnr), cwd(-1, tabnr(tabpage)))
+        end,
       })
 
       vim.api.nvim_create_autocmd("VimEnter", {
@@ -17,20 +24,15 @@ return {
           -- Only load the session if nvim was started with no args
           if vim.fn.argc(-1) == 0 then
             -- Save these to a different directory, so our manual sessions don't get polluted
-            resession.load(
-              vim.fn.getcwd(),
-              { dir = "dirsession", silence_errors = true }
-            )
+            resession.load(cwd(), { dir = "dirsession", silence_errors = true })
           end
         end,
       })
       vim.api.nvim_create_autocmd("VimLeavePre", {
         callback = function()
-          resession.save(
-            vim.fn.getcwd(),
-            { dir = "dirsession", notify = false }
-          )
-          resession.save("last")
+          resession.save_tab("last", { notify = false })
+          local name = cwd(-1, tabnr(curtab()))
+          resession.save_tab(name, { dir = "dirsession", notify = false })
         end,
       })
     end,
@@ -47,6 +49,7 @@ return {
         },
         scope_chdir = "tab",
       })
+      require("telescope").load_extension("projects")
     end,
   },
 }
--- lua/willothy/terminals.lua
+++ lua/willothy/terminals.lua
@@ -3,123 +3,118 @@ local M = {}
 local Terminal = require("toggleterm.terminal").Terminal
 
 M.float = Terminal:new({
-	display_name = "floating",
-	filetype = "toggleterm_float",
-	cmd = "zsh",
-	hidden = false,
-	direction = "float",
-	float_opts = {
-		border = "rounded",
-	},
-	close_on_exit = true,
+  display_name = "floating",
+  filetype = "toggleterm_float",
+  cmd = "zsh",
+  hidden = false,
+  direction = "float",
+  float_opts = {
+    border = "rounded",
+  },
+  close_on_exit = true,
 })
 
 M.main = Terminal:new({
-	display_name = "main",
-	cmd = "zsh",
-	hidden = false,
-	close_on_exit = true,
-	direction = "horizontal",
-	-- on_create = function(t)
-	-- 	local group = vim.api.nvim_create_augroup("term_autosize", { clear = true })
-	-- 	local buf = t.bufnr
-	-- 	vim.api.nvim_create_autocmd("TermEnter", {
-	-- 		buffer = buf,
-	-- 		group = group,
-	-- 		callback = function()
-	-- 			vim.api.nvim_win_set_height(t.window, vim.o.lines / 2)
-	-- 			local win = require("edgy").get_win(t.window)
-	-- 			if win then
-	-- 				-- expand window vertically
-	-- 				win.height = vim.o.lines / 2
-	-- 				win:resize()
-	-- 			end
-	-- 			-- t:resize(vim.o.lines / 2)
-	-- 		end,
-	-- 	})
-	-- 	vim.api.nvim_create_autocmd("BufLeave", {
-	-- 		buffer = buf,
-	-- 		group = group,
-	-- 		callback = function()
-	-- 			vim.api.nvim_win_set_height(t.window, 4)
-	-- 			local win = require("edgy").get_win(t.window)
-	-- 			if win then
-	-- 				-- expand window vertically
-	-- 				win.height = 4
-	-- 				win:resize()
-	-- 			end
-	-- 			-- t:resize(3)
-	-- 		end,
-	-- 	})
-	-- end,
+  display_name = "main",
+  cmd = "zsh",
+  hidden = false,
+  close_on_exit = true,
+  direction = "horizontal",
+  start_in_insert = false,
+  -- on_create = function(t)
+  -- 	local group = vim.api.nvim_create_augroup("term_autosize", { clear = true })
+  -- 	local buf = t.bufnr
+  -- 	vim.api.nvim_create_autocmd("TermEnter", {
+  -- 		buffer = buf,
+  -- 		group = group,
+  -- 		callback = function()
+  -- 			vim.api.nvim_win_set_height(t.window, vim.o.lines / 2)
+  -- 			local win = require("edgy").get_win(t.window)
+  -- 			if win then
+  -- 				-- expand window vertically
+  -- 				win.height = vim.o.lines / 2
+  -- 				win:resize()
+  -- 			end
+  -- 			-- t:resize(vim.o.lines / 2)
+  -- 		end,
+  -- 	})
+  -- 	vim.api.nvim_create_autocmd("BufLeave", {
+  -- 		buffer = buf,
+  -- 		group = group,
+  -- 		callback = function()
+  -- 			vim.api.nvim_win_set_height(t.window, 4)
+  -- 			local win = require("edgy").get_win(t.window)
+  -- 			if win then
+  -- 				-- expand window vertically
+  -- 				win.height = 4
+  -- 				win:resize()
+  -- 			end
+  -- 			-- t:resize(3)
+  -- 		end,
+  -- 	})
+  -- end,
 })
 
 M.py = Terminal:new({
-	cmd = "python3",
-	hidden = true,
+  cmd = "python3",
+  hidden = true,
 })
 
 M.lua = Terminal:new({
-	cmd = "lua",
-	hidden = true,
+  cmd = "lua",
+  hidden = true,
 })
 
 local edgy = (function()
-	local ok, res = pcall(require, "edgy")
-	return ok and res or nil
+  local ok, res = pcall(require, "edgy")
+  return ok and res or nil
 end)()
 
 ---@return Terminal
 function M.with()
-	local term = require("willothy.terminals").main
-	if term:is_open() then
-		if edgy then
-			local win = require("edgy").get_win(term.window)
-			if win and not win.visible then
-				win:open()
-			end
-		end
-	else
-		term:open()
-	end
-	return term
+  local term = require("willothy.terminals").main
+  if term:is_open() then
+    if edgy then
+      local win = require("edgy").get_win(term.window)
+      if win and not win.visible then win:open() end
+    end
+  else
+    term:open()
+  end
+  return term
 end
 
 function M.with_float()
-	local term = require("willothy.terminals").float
-	if term:is_open() then
-		if edgy then
-			local win = edgy.get_win(term.window)
-			if win and not win.visible then
-				win:open()
-			end
-		end
-	else
-		term:open()
-	end
-	return term
+  local term = require("willothy.terminals").float
+  if term:is_open() then
+    if edgy then
+      local win = edgy.get_win(term.window)
+      if win and not win.visible then win:open() end
+    end
+  else
+    term:open()
+  end
+  return term
 end
 
 function M.toggle()
-	local term = require("willothy.terminals").main
-	if term:is_open() then
-		if edgy then
-			local win = edgy.get_win(term.window)
-			if win and win.visible then
-				win:close()
-			elseif win then
-				win:open()
-			end
-		else
-			term:close()
-		end
-	else
-		term:open()
-	end
+  local term = require("willothy.terminals").main
+  if term:is_open() then
+    if edgy then
+      local win = edgy.get_win(term.window)
+      if win and win.visible then
+        win:close()
+      elseif win then
+        win:open()
+      end
+    else
+      term:close()
+    end
+  else
+    term:open()
+  end
 end
 
-function M.toggle_float()
-	M.float:toggle()
-end
+function M.toggle_float() M.float:toggle() end
 
 return M
