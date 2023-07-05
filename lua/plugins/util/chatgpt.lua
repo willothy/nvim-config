243e85bbc037048d296afc8927a10c24276ad127
Will Hopkins <willothyh@gmail.com>
2023-07-05 13:22:37 -0700

    lazy load chatgpt.nvim

--- lua/plugins/util/chatgpt.lua
+++ lua/plugins/util/chatgpt.lua
@@ -1,18 +1,19 @@
 return {
-	{
-		"dreamsofcode-io/ChatGPT.nvim",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-		lazy = true,
-		event = "VeryLazy",
-		enabled = false,
-		config = function()
-			require("chatgpt").setup({
-				async_api_key_cmd = "lpass show --password openai_key",
-			})
-		end,
-	},
+  {
+    "dreamsofcode-io/ChatGPT.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    lazy = true,
+    cmd = {
+      "ChatGPT",
+      "ChatGPTRunAs",
+      "ChatGPTExplain",
+    },
+    opts = {
+      async_api_key_cmd = "lpass show --password openai_key",
+    },
+  },
 }
