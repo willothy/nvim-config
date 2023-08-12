e380a39ff7060ea84701096f898fdcbbc47edc66
Will Hopkins <willothyh@gmail.com>
2023-08-11 23:28:57 -0700

    updates

--- lua/willothy/scratchpads/ui.lua
+++ lua/willothy/scratchpads/ui.lua
@@ -1,50 +1,94 @@
-local function set_layout(node)
-  local ty = node[1]
-  if ty == "leaf" then
-    local buf = node[2]
-    if type(buf) == "string" then
-      buf = vim.fn.bufadd(buf)
-      vim.api.nvim_buf_set_option(buf, "buflisted", true)
-    end
-    vim.api.nvim_set_current_buf(buf)
-  else
-    local winids = {}
-    for i in ipairs(node[2]) do
-      if i > 1 then
-        if ty == "row" then
-          vim.cmd("vsplit")
-        else
-          vim.cmd("split")
-        end
-      end
-      table.insert(winids, 1, vim.api.nvim_get_current_win())
-    end
-    for i, v in ipairs(node[2]) do
-      vim.api.nvim_set_current_win(winids[i])
-      set_layout(v)
-    end
-  end
+local function row(...)
+  return { "row", { ... } }
 end
 
-set_layout({
-  "row",
-  {
-    {
-      "leaf",
-      vim.fn.expand("%"),
-    },
-    {
-      "col",
-      {
-        {
-          "leaf",
-          "init.lua",
-        },
-        {
-          "leaf",
-          "init.lua",
-        },
-      },
-    },
-  },
+local function col(...)
+  return { "col", { ... } }
+end
+
+---@param buf number|string?
+local function win(buf)
+  return { "leaf", buf }
+end
+
+local layouts = setmetatable({}, {
+  __layouts = {},
+  __index = function(self, k)
+    return vim.deepcopy(getmetatable(self).__layouts[k])
+  end,
+  __newindex = function(self, k, v)
+    getmetatable(self).__layouts[k] = v
+  end,
+})
+
+--
+-- |‾‾‾‾‾|‾‾‾‾‾|
+-- |     |  B  |
+-- |  A  |-----|
+-- |     |  C  |
+-- |_____|_____|
+--
+layouts.tile_right = row(
+  -- A
+  win(),
+  col(
+    -- B
+    win(),
+    -- C
+    win()
+  )
+)
+
+--
+-- |‾‾‾‾‾|‾‾‾‾‾|
+-- |  A  |     |
+-- |-----|  C  |
+-- |  B  |     |
+-- |_____|_____|
+--
+layouts.tile_left = row(
+  col(
+    -- A
+    win(),
+    -- B
+    win()
+  ),
+  -- C
+  win()
+)
+
+-- assign buffers in depth-first order to empty leaves, create a scratch buffer for any remaining empty windows,
+-- and create the layout.
+local function set_layout(name, bufs)
+  if not bufs then
+    bufs = vim.iter(vim.api.nvim_list_bufs()):filter(function(buf)
+      return vim.api.nvim_buf_get_option(buf, "buflisted")
+    end)
+  else
+    bufs = vim.iter(bufs)
+  end
+  local tmp_buf
+
+  local function process(node)
+    if node[1] == "leaf" then
+      node[2] = bufs:next()
+      if node[2] == nil then
+        if not tmp_buf then
+          tmp_buf = vim.api.nvim_create_buf(false, true)
+        end
+        node[2] = tmp_buf
+      end
+    else
+      vim.iter(node[2]):each(process)
+    end
+    return node
+  end
+
+  local tree = process(layouts[name])
+  vim.api.nvim_tabpage_set_layout(0, tree)
+end
+
+set_layout("tile_right", {
+  "init.lua",
+  "README.md",
 })
