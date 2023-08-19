56b2ce9c9fff398d648dd11c167c346c0aee46d0
Will Hopkins <willothyh@gmail.com>
2023-08-19 00:33:40 -0700

    plugin util updaets

--- lua/willothy/modules/utils/plugins.lua
+++ lua/willothy/modules/utils/plugins.lua
@@ -1,19 +1,14 @@
 local M = {}
 
-function M.list_plugins()
-  local plugins = vim
-    .iter(require("lazy").plugins())
-    :map(function(p)
-      return p.name
-    end)
-    :fold("\n", function(acc, name)
-      return acc .. name .. "\n"
-    end)
-
-  willothy.fn.popup(plugins, "installed forks")
+function M.iter()
+  return vim.iter(require("lazy").plugins())
 end
 
-function M.list_forks()
+function M.is_mine(p)
+  return vim.startswith(p[1] or "", "willothy")
+end
+
+function M.is_fork(p)
   local my_plugins = {
     ["minimus"] = true,
     ["flatten.nvim"] = true,
@@ -24,23 +19,95 @@ function M.list_forks()
     ["nvim-cokeline"] = true,
     ["moveline.nvim"] = true,
   }
+  return not my_plugins[p.name]
+end
 
-  local forks = vim
-    .iter(require("lazy").plugins())
-    :filter(function(p)
-      return vim.startswith(p[1], "willothy")
-    end)
-    :map(function(p)
-      return p.name
-    end)
-    :filter(function(name)
-      return not my_plugins[name]
-    end)
-    :fold("\n", function(acc, name)
-      return acc .. name .. "\n"
+function M.is_local(p)
+  return not vim.startswith(p.dir, vim.fn.stdpath("data"))
+end
+
+function M.get_name(p)
+  return p.name
+end
+
+function M.has_url(p)
+  return p.url ~= nil
+end
+
+function M.any(...)
+  local args = { ... }
+  return function(p)
+    for _, f in ipairs(args) do
+      if f(p) then
+        return true
+      end
+    end
+    return false
+  end
+end
+
+function M.all(...)
+  local args = { ... }
+  return function(p)
+    for _, f in ipairs(args) do
+      if not f(p) then
+        return false
+      end
+    end
+    return true
+  end
+end
+
+function M.not_(f)
+  return function(p)
+    return not f(p)
+  end
+end
+
+function M.list()
+  local plugins = M.iter():map(M.get_name):fold("\n", function(acc, name)
+    return acc .. name .. "\n"
+  end)
+
+  willothy.fn.popup(plugins, "installed plugins")
+end
+
+function M.list_local()
+  local local_plugins = M.iter()
+    :filter(M.is_local)
+    :filter(M.has_url)
+    :map(M.get_name)
+    :fold("\n", function(acc, p)
+      return acc .. p .. "\n"
     end)
 
-  willothy.fn.popup(forks, "installed forks")
+  willothy.fn.popup(local_plugins, "local plugins")
+end
+
+function M.list_mine()
+  local my_plugins = M.iter()
+    :filter(M.is_mine)
+    :filter(M.has_url)
+    :filter(M.not_(M.is_fork))
+    :map(M.get_name)
+    :fold("\n", function(acc, p)
+      return acc .. p .. "\n"
+    end)
+
+  willothy.fn.popup(my_plugins, "plugins by @willothy")
+end
+
+function M.list_forks()
+  local forks = M.iter()
+    :filter(M.any(M.is_mine, M.is_local))
+    :filter(M.has_url)
+    :filter(M.is_fork)
+    :map(M.get_name)
+    :fold("\n", function(acc, p)
+      return acc .. p .. "\n"
+    end)
+
+  willothy.fn.popup(forks, "installed plugin forks")
 end
 
 return M
