adf065753d7ae2ed724990880012dcc6c3290589
Will Hopkins <willothyh@gmail.com>
2023-08-21 17:34:06 -0700

    small updates

--- lua/configs/lsp/sourcegraph.lua
+++ lua/configs/lsp/sourcegraph.lua
@@ -8,20 +8,20 @@ vim.system(
     local status, out, err = obj.code, obj.stdout, obj.stderr
 
     if status ~= 0 then
-      vim.api.nvim_err_Writeln(("Sourcegraph: %s"):format(err))
+      vim.api.nvim_err_writeln(("Sourcegraph: %s"):format(err))
       return
     end
 
     local sg = require("sg")
     local auth = require("sg.auth")
 
-    sg.setup({
-      on_attach = require("configs.lsp").lsp_attach,
-      auth_strategy = { "environment-variables", "nvim" },
-    })
     auth.set_nvim_auth({
       endpoint = "https://sourcegraph.com/",
       token = trim(out),
+    })
+    sg.setup({
+      on_attach = require("configs.lsp").lsp_attach,
+      auth_strategy = { "environment-variables", "nvim" },
     })
   end)
 )
