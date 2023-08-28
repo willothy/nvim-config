e446617196d5cf6be5a4254ee3ee81a93f33ffb0
Will Hopkins <willothyh@gmail.com>
2023-08-28 03:32:13 -0700

    lazy edit cmd +E

--- init.lua
+++ init.lua
@@ -4,9 +4,29 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
 local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 local conf_path = vim.fn.stdpath("config")
+---@cast conf_path string
 
 vim.opt.rtp:prepend(lazy_path)
+
+vim.api.nvim_create_user_command(
+  "E",
+  vim.schedule_wrap(function(o)
+    vim.print(o.fargs)
+    for i, file in vim.iter(o.fargs):enumerate() do
+      local buf = vim.fn.bufadd(file)
+      if buf then
+        vim.fn.bufload(buf)
+        vim.bo[buf].buflisted = true
+        if i == 1 then
+          vim.api.nvim_set_current_buf(buf)
+        end
+      end
+    end
+  end),
+  { nargs = "*" }
+)
 
 require("lazy").setup({
   { import = "plugins" },
