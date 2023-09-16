4e70fd166e96378c60cf0891a2cba78d48fa445f
Will Hopkins <willothyh@gmail.com>
2023-09-15 18:27:13 -0700

    js and scss ftplugins

---
+++ after/ftplugin/javascript.lua
@@ -0,0 +1,14 @@
+
+local bo = vim.bo
+local wo = vim.wo
+
+bo.tabstop = 2
+bo.shiftwidth = 2
+bo.softtabstop = -1
+bo.expandtab = true
+bo.smartindent = false
+
+wo.wrap = false
+wo.numberwidth = 1
+wo.number = true
+wo.relativenumber = true
