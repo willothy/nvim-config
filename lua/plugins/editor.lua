fc88f2cac0598cce4944fe28a9ac3ec8f0873c56
Will Hopkins <willothyh@gmail.com>
2023-09-30 06:11:01 -0700

    update plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -122,7 +122,6 @@ return {
       "dhruvmanila/browser-bookmarks.nvim",
       {
         -- "nvim-telescope/telescope-frecency.nvim",
-        -- commit = "fbda5d91d6e787f5977787fa4a81da5c8e22160a",
         "willothy/telescope-frecency.nvim",
         branch = "fix-workspace-filter",
         -- dir = "~/projects/lua/telescope-frecency.nvim",
