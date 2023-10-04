91630a4cb30e09019c6f20780ff95f16ec6938a4
Will Hopkins <willothyh@gmail.com>
2023-10-03 23:22:22 -0700

    rewrite resession edgy extension without vim.iter

--- lua/resession/extensions/edgy.lua
+++ lua/resession/extensions/edgy.lua
@@ -1,17 +1,10 @@
 local M = {}
 
 function M.on_save()
-  return vim
-    .iter(vim.api.nvim_tabpage_list_wins(0))
-    :map(function(win)
-      return require("edgy").get_win(win)
-    end)
-    ---@param win Edgy.Window
-    :filter(function(win)
-      return win.view.open ~= nil
-    end)
-    ---@param win Edgy.Window
-    :map(function(win)
+  local wins = {}
+  for _, winid in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    local win = require("edgy").get_win(winid)
+    if win and win.view.open then
       local open = win.view.open
       if type(open) == "string" then
         open = function()
@@ -19,27 +12,28 @@ function M.on_save()
         end
       end
       open = string.dump(open --[[@as fun()]])
-      return {
+      table.insert(wins, {
         ft = win.view.ft,
         open = open,
-      }
-    end)
-    :totable()
+      })
+    end
+  end
+  return wins
 end
 
 function M.on_pre_load(data)
   local win = vim.api.nvim_get_current_win()
   local animate = require("edgy.config").animate.enabled
   require("edgy.config").animate.enabled = false
-  vim.iter(data or {}):each(function(wininfo)
+  for _, wininfo in ipairs(data or {}) do
     local f = loadstring(wininfo.open)
     if f then
       local ok = pcall(f)
       if not ok then
-        vim.notify("[recession] Failed to open " .. wininfo.ft)
+        vim.notify("[resession] Failed to open " .. wininfo.ft, "warn")
       end
     end
-  end)
+  end
   require("edgy.config").animate.enabled = animate
   vim.api.nvim_set_current_win(win)
 end
