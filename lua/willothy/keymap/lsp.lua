0ba47d11e32dd91c5e996cdb4decb9460982aa40
Will Hopkins <willothyh@gmail.com>
2023-10-07 01:05:27 -0700

    lazy-ify lsp keymaps so that lsp.buf.* can be overridden

--- lua/willothy/keymap/lsp.lua
+++ lua/willothy/keymap/lsp.lua
@@ -6,15 +6,15 @@ local wk = require("which-key")
 
 wk.register({
   name = "lsp",
-  a = bind(vim.lsp.buf.code_action):with_desc("code actions"),
-  r = bind(vim.lsp.buf.references):with_desc("references"),
-  d = bind(vim.lsp.buf.definition):with_desc("definitions"),
-  D = bind(vim.lsp.buf.declaration):with_desc("declarations"),
-  T = bind(vim.lsp.buf.type_definition):with_desc("type definitions"),
-  i = bind(vim.lsp.buf.implementation):with_desc("implementations"),
-  h = bind(vim.lsp.buf.signature_help):with_desc("signature help"),
-  O = bind(vim.lsp.buf.outgoing_calls):with_desc("outgoing calls"),
-  I = bind(vim.lsp.buf.incoming_calls):with_desc("incoming calls"),
+  a = bind(vim.lsp.buf, "code_action"):with_desc("code actions"),
+  r = bind(vim.lsp.buf, "references"):with_desc("references"),
+  d = bind(vim.lsp.buf, "definition"):with_desc("definitions"),
+  D = bind(vim.lsp.buf, "declaration"):with_desc("declarations"),
+  T = bind(vim.lsp.buf, "type_definition"):with_desc("type definitions"),
+  i = bind(vim.lsp.buf, "implementation"):with_desc("implementations"),
+  h = bind(vim.lsp.buf, "signature_help"):with_desc("signature help"),
+  O = bind(vim.lsp.buf, "outgoing_calls"):with_desc("outgoing calls"),
+  I = bind(vim.lsp.buf, "incoming_calls"):with_desc("incoming calls"),
   n = {
     function()
       vim.api.nvim_feedkeys(
