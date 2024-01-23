93d422f7ea46151de47040347aa1b316014707d3
Will Hopkins <willothyh@gmail.com>
2024-01-23 04:53:16 -0800

    tiny spec update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -120,7 +120,8 @@ return {
   -- COMPLETION --
   {
     "hrsh7th/nvim-cmp",
-    dir = "~/projects/lua/nvim-cmp/",
+    -- "willothy/nvim-cmp",
+    -- dir = "~/projects/lua/nvim-cmp/",
     dependencies = {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
