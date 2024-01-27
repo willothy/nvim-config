fb2482cd50017ba3d47706a79dd95adc1164a701
Will Hopkins <willothyh@gmail.com>
2024-01-26 17:24:00 -0800

    updates

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -218,7 +218,7 @@ return {
   {
     "willothy/flatten.nvim",
     branch = "guest-data",
-    dir = "~/projects/lua/flatten/",
+    -- dir = "~/projects/lua/flatten/",
     -- cond = true,
     lazy = false,
     priority = 1000,
