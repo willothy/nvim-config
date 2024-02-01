d688174af1fd71d5ade8764051ddf32c97f61658
Will Hopkins <willothyh@gmail.com>
2024-02-01 02:25:42 -0800

    update plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -54,11 +54,6 @@ return {
     "3rd/image.nvim",
     event = "VeryLazy",
     config = function()
-      package.path = package.path
-        .. ";"
-        .. "/usr/share/lua/5.1/?/init.lua;"
-        .. ";"
-        .. "/usr/share/lua/5.1/?.lua;"
       require("image").setup({})
     end,
   },
