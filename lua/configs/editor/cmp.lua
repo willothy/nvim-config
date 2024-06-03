4a8af93b7808237418798c7f1bfff9c9501b2aa3
Will Hopkins <willothyh@gmail.com>
2024-06-02 17:23:37 -0700

    add lazydev completion source

--- lua/configs/editor/cmp.lua
+++ lua/configs/editor/cmp.lua
@@ -84,6 +84,7 @@ local opts = {
     { name = "nvim_px_to_rem" },
     { name = "copilot", max_item_count = 1 },
     { name = "snippets" },
+    { name = "lazydev" },
     {
       name = "nvim_lsp" --[[ , max_item_count = 40 ]],
     },
