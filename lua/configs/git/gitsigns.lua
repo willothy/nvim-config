1b9b16be063163db3d9d5f514d75fa166d1c54b2
Will Hopkins <willothyh@gmail.com>
2025-06-08 12:55:35 -0700

    fix too many open files for gitsigns in big monorepo

--- lua/configs/git/gitsigns.lua
+++ lua/configs/git/gitsigns.lua
@@ -8,29 +8,41 @@ git._set_version = function(version)
   pcall(_set_version, version)
 end
 
-gitsigns.setup({
-  signs = {
-    untracked = { text = bar },
-    add = { text = bar },
-    change = { text = bar },
-    delete = { text = bar },
-    topdelete = { text = bar },
-    changedelete = { text = bar },
-  },
-  preview_config = {
-    focusable = false,
-    -- Options passed to nvim_open_win
-    border = "single",
-    style = "minimal",
-    relative = "cursor",
-    row = 0,
-    col = 1,
-  },
-  trouble = true,
-  signcolumn = true,
-  update_debounce = 250,
-  -- _extmark_signs = false,
-  -- on_attach = vim.schedule_wrap(function(bufnr)
-  --   vim.cmd.GitConflictRefresh()
-  -- end),
-})
+local function do_setup()
+  gitsigns.setup({
+    signs = {
+      untracked = { text = bar },
+      add = { text = bar },
+      change = { text = bar },
+      delete = { text = bar },
+      topdelete = { text = bar },
+      changedelete = { text = bar },
+    },
+    preview_config = {
+      focusable = false,
+      -- Options passed to nvim_open_win
+      border = "single",
+      style = "minimal",
+      relative = "cursor",
+      row = 0,
+      col = 1,
+    },
+    trouble = true,
+    signcolumn = true,
+    update_debounce = 250,
+    -- _extmark_signs = false,
+    -- on_attach = vim.schedule_wrap(function(bufnr)
+    --   vim.cmd.GitConflictRefresh()
+    -- end),
+  })
+end
+
+if vim.g.did_very_lazy then
+  vim.schedule(do_setup)
+else
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "VeryLazy",
+    once = true,
+    callback = vim.schedule_wrap(do_setup),
+  })
+end
